83fbfb9ac79fb0134ea85c009143b3374ba575dd
EYV-SITE <jesus.escudero@eyvsolutions.cl>
2026-06-16 21:28:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const visor = document.getElementById('contenedorPDF');
 const iframeInventario = document.getElementById('iframeInventario');
 
 // Enlaces dinámicos construidos con tu ID de Google Drive y tu Hoja GID
-const urlEditable = "https://docs.google.com/spreadsheets/d/1i_ZB-IuV3Pt1tiE4U8_9uEtLkNdRIZ3B2AXo_y5C6SM/edit?rm=minimal&gid=951590461";
+const urlEditable = "https://docs.google.com/spreadsheets/d/1i_ZB-IuV3Pt1tiE4U8_9uEtLkNdRIZ3B2AXo_y5C6SM/edit?rm=minimal&gid=689203295";
 const urlPDFEnVivo = "https://docs.google.com/spreadsheets/d/1i_ZB-IuV3Pt1tiE4U8_9uEtLkNdRIZ3B2AXo_y5C6SM/export?format=pdf&gid=951590461";
 
 // 1. Al presionar el botón principal del portal, abrimos el cuadro elegante
